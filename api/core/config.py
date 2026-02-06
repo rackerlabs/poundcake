d07f3580900bdb34ef64959e5871067c0916da07
Jake Briggs <briggs679@gmail.com>
2026-02-06 08:44:19 -0600

    fix: Update app_version to 2.0.53 in config

--- api/core/config.py
+++ api/core/config.py
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     # Application Settings
     # ==========================================================================
     app_name: str = "PoundCake"
-    app_version: str = "1.0.0"
+    app_version: str = "2.0.53"
     debug: bool = False
 
     # API Server
