39f2e4141cafb5570413280101cba306a90ef569
Chris Breu <chris.breu@rackspace.com>
2026-02-20 16:53:13 -0600

    fix: new env specific installer script

--- install/install-helm.sh
+++ install/install-helm.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
 if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
-  exec "$PROJECT_ROOT/helm/bin/install-poundcake.sh" "$@"
+  exec "$PROJECT_ROOT/helm/bin/install-poundcake-with-env.sh" "$@"
 fi
 
-exec "$PROJECT_ROOT/helm/bin/install-poundcake.sh" "$@"
+exec "$PROJECT_ROOT/helm/bin/install-poundcake-with-env.sh" "$@"
